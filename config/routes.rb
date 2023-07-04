74825cfd6613d6a058ec7976e2cdf58f906532b7
Marcel Schmidt <marcel.schmidt@sapiens.com>
2023-07-04 12:10:44 +0200

    Build stock lookup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "welcome#index"
+  root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
 end
