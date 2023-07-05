fc8bb8f9c0c0771f40a04f5914dec1c366025057
Marcel Schmidt <marcel.schmidt@sapiens.com>
2023-07-05 19:01:55 +0200

    remove freind/stop following functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,22 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
+
+  def search
+    @friends = current_user.friends
+
+    if params[:friend].present?
+      @searched_friends = User.search(params[:friend])
+      @searched_friends = current_user.except_current_user(@searched_friends)
+      if @searched_friends
+        render 'users/my_friends'
+      else
+        flash[:alert] = "Couldn't find user"
+        redirect_to my_friends_path
+      end
+    else
+      flash[:alert] = "Please enter a friend name or email to search"
+      redirect_to my_friends_path
+    end
+  end
 end
