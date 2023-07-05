de6c45a3b7cf898dd82a43d044e0b8d3edc476f6
Marcel Schmidt <marcel.schmidt@sapiens.com>
2023-07-05 14:47:36 +0200

    Cleanup application layout

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,7 +2,11 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
+      @tracked_stocks = current_user.stocks
       if @stock
+        # respond_to do |format|
+        #   format.js { render partial 'users/result' }
+        # end
         render 'users/my_portfolio'
       else
         flash[:alert] = "Please enter a valid symbol to search"
